3a3e3a4263a532e7f7bb21ba93cc9d117afc39b9
monster <monster@hamsterbase.com>
2023-06-08 23:37:45 +0800

    fix: Resolve issue with failed initialization of the page

--- src/application.tsx
+++ src/application.tsx
@@ -2,16 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { MemoTalk, MemoTalkCore } from './core/memo-talk-core.ts';
 import { MemoTalkContainer } from './memo-talk.tsx';
 
-const memoTalkCore = new MemoTalkCore();
-
-memoTalkCore.onUpdate(() => {
-  localStorage.setItem('memoTalks', memoTalkCore.encode());
-});
-
-if (localStorage.getItem('memoTalks')) {
-  memoTalkCore.merge(localStorage.getItem('memoTalks') as string);
-}
-
 export interface Props {
   memoTalkCore: MemoTalkCore;
 }
@@ -27,8 +17,8 @@ export const App: React.FC<Props> = (props) => {
     <MemoTalkContainer
       memoTalks={memoTalks}
       onCreateMemoTalk={(content: string) => {
-        memoTalkCore.createMemoTalk(content);
-        setMemoTalks(memoTalkCore.getMemoTalkList());
+        props.memoTalkCore.createMemoTalk(content);
+        setMemoTalks(props.memoTalkCore.getMemoTalkList());
       }}
     />
   );
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,14 @@ import { MemoTalkCore } from './core/memo-talk-core.ts';
 
 const memoTalkCore = new MemoTalkCore();
 
+if (localStorage.getItem('memoTalks')) {
+  memoTalkCore.merge(localStorage.getItem('memoTalks') as string);
+}
+
+memoTalkCore.onUpdate(() => {
+  localStorage.setItem('memoTalks', memoTalkCore.encode());
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App memoTalkCore={memoTalkCore}></App>
