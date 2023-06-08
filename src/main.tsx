bd338a064f4d16fd2d72a0fe8266b6d547f6b552
monster <monster@hamsterbase.com>
2023-06-08 23:29:29 +0800

    fix: rename app -> application

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './app.tsx';
+import { App } from './application.tsx';
 import { MemoTalkCore } from './core/memo-talk-core.ts';
 
 const memoTalkCore = new MemoTalkCore();
