9b215a0d554f9c0af680d7e3eab58fecd4042e7d
monster <monster@hamsterbase.com>
2023-06-08 22:25:53 +0800

    feat: impl MemoTalkCore

--- src/core/memo-talk-core.ts
+++ src/core/memo-talk-core.ts
@@ -1,3 +1,6 @@
+import { nanoid } from "nanoid";
+import * as Y from "yjs";
+import { fromUint8Array, toUint8Array } from "js-base64";
 export interface MemoTalk {
   id: string;
   content: string;
@@ -9,7 +12,7 @@ export interface MemoTalk {
  * 保存的时候 encode 为字符串
  * 可以合并别的 MemoTalkCore, 入参为字符串
  */
-export interface MemoTalkCore {
+export interface IMemoTalkCore {
   createMemoTalk(content: string): string;
 
   getMemoTalkById(id: string): MemoTalk;
@@ -26,4 +29,62 @@ export interface MemoTalkCore {
   merge(data: string): void;
 }
 
-class NoteCore implements NoteCore {}
+const enum YDocKey {
+  /**
+   * 避免 id 重复
+   */
+  id = "id",
+  memoTalks = "memoTalks",
+}
+
+export class MemoTalkCore implements IMemoTalkCore {
+  private ydoc: Y.Doc;
+  constructor() {
+    this.ydoc = new Y.Doc();
+  }
+
+  createMemoTalk(content: string): string {
+    if (typeof content !== "string") {
+      throw new Error("content must be string");
+    }
+    const idMap = this.ydoc.getMap(YDocKey.id);
+    let id: string = nanoid();
+    while (idMap.has(id)) {
+      id = nanoid();
+    }
+    const memoTalksArray = this.ydoc.getArray<string>(YDocKey.memoTalks);
+
+    const memoTalk = this.ydoc.getMap(id);
+    memoTalk.set("content", content ?? "");
+    memoTalk.set("createTime", Date.now());
+    memoTalksArray.push([id]);
+    return id;
+  }
+
+  getMemoTalkById(id: string): MemoTalk {
+    const memoTalk = this.ydoc.getMap(id);
+    return {
+      id,
+      content: memoTalk.get("content") as string,
+      createTime: memoTalk.get("createTime") as number,
+    };
+  }
+
+  getMemoTalkList(): MemoTalk[] {
+    const memoTalksArray = this.ydoc.getArray<string>(YDocKey.memoTalks);
+    return memoTalksArray.map((id) => this.getMemoTalkById(id));
+  }
+
+  deleteMemoTalkById(id: string): void {
+    const memoTalksArray = this.ydoc.getArray<string>(YDocKey.memoTalks);
+    memoTalksArray.delete(memoTalksArray.toArray().indexOf(id));
+  }
+
+  encode(): string {
+    return fromUint8Array(Y.encodeStateAsUpdate(this.ydoc));
+  }
+
+  merge(data: string): void {
+    Y.applyUpdate(this.ydoc, toUint8Array(data));
+  }
+}
