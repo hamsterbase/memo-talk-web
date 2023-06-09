d7024d70c6c63cca47aa550175504aa95e22f3eb
monster <monster@hamsterbase.com>
2023-06-09 21:56:14 +0800

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        settings: resolve(__dirname, 'settings/index.html'),
+      },
+    },
+  },
   plugins: [react()],
-})
+});
